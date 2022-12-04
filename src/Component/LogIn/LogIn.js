0ef430b182e305d60fbf0c852892d161c78304f3
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-12-05 01:11:15 +0600

    add login page

--- src/Component/LogIn/LogIn.js
+++ src/Component/LogIn/LogIn.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import Img from "../../images/FormImage.jpg";
+import { Link } from "react-router-dom";
+import logo from "../../images/logo.jpg";
+import { FaArrowRight } from "react-icons/fa";
 
 const LogIn = () => {
-    return (
-        <div>
-            <h1>login</h1>
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("example"));
+  return (
+    <div className="flex items-center gap-36 mx-auto justify-center">
+      <div>
+        <img src={logo} alt="" />
+        <img className="w-[612]px h-[437px]" src={Img} alt="" />
+      </div>
+      <div className="w-[516px] h-[630px] bg-white shadow-2xl p-16">
+        <h2 className="text-2xl font-semibold text-center ">SignUp Form</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className=" mt-28">
+            <input
+              name="email"
+              required
+              placeholder="Write Email Address"
+              type="email"
+              className="font-normal pl-4 text-md leading-5 text-gray-400  w-full focus:outline-none border-b border-gray-300"
+            />
+          </div>
+          {errors.exampleRequired && <span>This field is required</span>}
+          <div className=" mt-16">
+            <input
+              name="password"
+              required
+              placeholder="Write Password"
+              type="password"
+              className="font-normal pl-4 text-md leading-5 text-gray-400  w-full focus:outline-none border-b border-gray-300"
+            />
+          </div>
+          {errors.exampleRequired && <span>This field is required</span>}
+          <div className="flex justify-center items-center mt-20">
+            <button className=" gap-3 drop-shadow-lg hover:bg-white hover:text-blue-500 hover:bg-gradient-to-r hover:from-white hover:to-white hover:border hover:border-blue-500 flex w-36 justify-center rounded-2xl text-white font-medium text-md bg-gradient-to-r from-blue-500 to-blue-500   h-12 items-center">
+              Next Step
+              <FaArrowRight></FaArrowRight>
+            </button>
+          </div>
+          <div className=" flex gap-2 mt-28 justify-end items-center ">
+            <h1>Already have an account? </h1>
+            <Link>
+              <span className="uppercase underline font-semibold leading-4 text-blue-500">
+                login here!
+              </span>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
--- src/Component/SignUp/SignUp.js
+++ src/Component/SignUp/SignUp.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import formImg from "../../images/img.jpg";
+import Img from "../../images/FormImage.jpg";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.jpg";
 import { FaArrowRight } from "react-icons/fa";
 
 const SignUp = () => {
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(2);
   const {
     register,
     handleSubmit,
@@ -15,50 +15,119 @@ const SignUp = () => {
   } = useForm();
   const onSubmit = (data) => console.log(data);
 
-  console.log(watch("example")); // watch input value by passing the name of it
+  console.log(watch("example")); 
   return (
     <div className="flex items-center gap-36 mx-auto justify-center">
       <div>
         <img src={logo} alt="" />
-        <img className="w-[612]px h-[437px]" src={formImg} alt="" />
+        <img className="w-[612]px h-[437px]" src={Img} alt="" />
       </div>
       <div className="w-[516px] h-[630px] bg-white shadow-2xl p-16">
         <h2 className="text-2xl font-semibold text-center ">SignUp Form</h2>
         <form onSubmit={handleSubmit(onSubmit)}>
-          {/* register your input into the hook by invoking the "register" function */}
-          <div className=" mt-28">
-            <input
-              name="fristName"
-              required
-              placeholder="Write First Name"
-              type="text"
-              className="font-normal text-md leading-5 text-gray-400  w-full focus:outline-none border-b border-gray-300"
-            />
-          </div>
-          {errors.exampleRequired && <span>This field is required</span>}
-          <div className=" mt-16">
-            <input
-              name="lastName"
-              required
-              placeholder="Write Last Name"
-              type="text"
-              className="font-normal text-md leading-5 text-gray-400  w-full focus:outline-none border-b border-gray-300"
-            />
-          </div>
-          {errors.exampleRequired && <span>This field is required</span>}
-          <div className="flex justify-center items-center mt-20">
-            <button className=" gap-3 drop-shadow-lg flex w-36 justify-center rounded-2xl text-white font-medium text-md bg-gradient-to-r from-blue-500 to-blue-500   h-12 items-center">
-              Next Step
-              <FaArrowRight></FaArrowRight>
-            </button>
-          </div>
+          {step === 0 && (
+            <>
+              <div className=" mt-28">
+                <input
+                  name="fristName"
+                  required
+                  placeholder="Write First Name"
+                  type="text"
+                  className="font-normal pl-4 text-md leading-5 text-gray-400  w-full focus:outline-none border-b border-gray-300"
+                />
+              </div>
+              {errors.exampleRequired && <span>This field is required</span>}
+              <div className=" mt-16">
+                <input
+                  name="lastName"
+                  required
+                  placeholder="Write Last Name"
+                  type="text"
+                  className="font-normal pl-4 text-md leading-5 text-gray-400  w-full focus:outline-none border-b border-gray-300"
+                />
+              </div>
+              {errors.exampleRequired && <span>This field is required</span>}
+              <div className="flex justify-center items-center mt-20">
+                <button className=" gap-3 drop-shadow-lg hover:bg-white hover:text-blue-500 hover:bg-gradient-to-r hover:from-white hover:to-white hover:border hover:border-blue-500 flex w-36 justify-center rounded-2xl text-white font-medium text-md bg-gradient-to-r from-blue-500 to-blue-500   h-12 items-center">
+                  Next Step
+                  <FaArrowRight></FaArrowRight>
+                </button>
+              </div>
+              <div className=" flex gap-2 mt-28 justify-end items-center ">
+                <h1>Already have an account? </h1>
+                <Link>
+                  <span className="uppercase underline font-semibold leading-4 text-blue-500">
+                    login here!
+                  </span>
+                </Link>
+              </div>
+            </>
+          )}
+          {step === 1 && (
+            <>
+              <div className=" mt-28 flex gap-1 ">
+                <input
+                  name="number"
+                  readOnly
+                  defaultValue={"+880"}
+                  placeholder="+880"
+                  type="number"
+                  className="font-normal pl-4 w-20 text-md leading-5 text-gray-400 focus:outline-none border-b border-gray-300"
+                />
+                <input
+                  name="number"
+                  required
+                  placeholder="1xxxxxxxxx"
+                  type="number"
+                  className="font-normal  text-md leading-5 text-gray-400  w-full focus:outline-none border-b border-gray-300"
+                />
+              </div>
+              {errors.exampleRequired && <span>This field is required</span>}
+              <div className=" mt-16">
+                <input
+                  name="lastName"
+                  required
+                  placeholder="Write Email Address"
+                  type="email"
+                  className="font-normal pl-4 text-md leading-5 text-gray-400  w-full focus:outline-none border-b border-gray-300"
+                />
+              </div>
+              {errors.exampleRequired && <span>This field is required</span>}
+              <div className="flex gap-24 items-center mt-20">
+                <Link>
+                  <span className="font-bold text-gray-500 ">Back</span>
+                </Link>
+                <button className=" gap-3 drop-shadow-lg hover:bg-white hover:text-blue-500 hover:bg-gradient-to-r hover:from-white hover:to-white hover:border hover:border-blue-500 flex w-36 justify-center rounded-2xl text-white font-medium text-md bg-gradient-to-r from-blue-500 to-blue-500   h-12 items-center">
+                  Next Step
+                  <FaArrowRight></FaArrowRight>
+                </button>
+              </div>
+            </>
+          )}
+          {step === 2 && (
+            <>
+              <div className=" mt-28 ">
+                <input
+                  name="password"
+                  required
+                  placeholder="Write Password"
+                  type="password"
+                  className="font-normal pl-4 text-md leading-5 text-gray-400  w-full focus:outline-none border-b border-gray-300"
+                />
+              </div>
+              <small className="text-gray-400 pl-4">Your password must be 8 character</small>
+              {errors.exampleRequired && <span>This field is required</span>}
+              <div className="flex gap-24 items-center mt-20">
+                <Link>
+                  <span className="font-bold text-gray-500 ">Back</span>
+                </Link>
+                <button className=" gap-3 drop-shadow-lg hover:bg-white hover:text-blue-500 hover:bg-gradient-to-r hover:from-white hover:to-white hover:border hover:border-blue-500 flex w-28 justify-center rounded-2xl text-white font-medium text-md bg-gradient-to-r from-blue-500 to-blue-500   h-12 items-center">
+                  Sign Up
+                </button>
+              </div>
+            </>
+          )}
         </form>
-        <div className=" flex gap-2 mt-28 justify-end items-center ">
-          <h1>Already have an account? </h1>
-          <Link>
-            <span className="uppercase underline font-semibold leading-4 text-blue-500">login here!</span>
-          </Link>
-        </div>
       </div>
     </div>
   );
